4183bde824239ce6239be3a9643f1b6661adb6ad
Bijay Yadav <bijay@idealaya.com.np>
2014-03-31 16:18:05 +0545

    theme description updated

--- inc/admin/footer.php
+++ inc/admin/footer.php
@@ -1,12 +1,12 @@
 <?php
-if ( ! defined('ABSPATH')) exit('restricted access');
+if ( ! defined( 'ABSPATH' ) ) exit( 'restricted access' );
 
 $link = '';
-if(isset($_GET['sam-page']) && $_GET['sam-page']!= '') {
+if( isset( $_GET['sam-page'] ) && $_GET['sam-page']!= '' ) {
     $link = '&sam-page='.$_GET['sam-page'];
 }
 ?>
-<a href="themes.php?page=<?php echo $_GET['page'] ?><?php echo $link ?>&action=restore" class="button4 sampression-restore"><?php echo _e('Restore all to default settings', 'sampression'); ?></a>
+<a href="themes.php?page=<?php echo $_GET['page'] ?><?php echo $link ?>&action=restore" class="button4 sampression-restore"><?php echo _e( 'Restore all to default settings', 'sampression' ); ?></a>
 </div>
 <!-- #sam-main-content-->
 </div>
--- inc/admin/header.php
+++ inc/admin/header.php
@@ -1,20 +1,20 @@
-<?php if ( ! defined('ABSPATH')) exit('restricted access');?>
+<?php if ( ! defined( 'ABSPATH' ) ) exit( 'restricted access' );?>
 <div id="sam-wrapper">
     <div id="header" class=" clearfix">
         <div id="logo">
-            <h2><?php _e('nayá lite', 'sampression');?></h2>
-            <span class="tagline"><?php _e('a new theme', 'sampression');?></span>
+            <h2><?php _e( 'nayá lite', 'sampression' );?></h2>
+            <span class="tagline"><?php _e( 'a new theme', 'sampression' );?></span>
         </div>
         <div class="sam-version">
-            <span><?php _e('version 1.0', 'sampression');?></span>
-            <span><?php _e('framework 1.0', 'sampression');?></span>
+            <span><?php _e( 'version 1.0', 'sampression' );?></span>
+            <span><?php _e( 'framework 1.0', 'sampression' );?></span>
         </div>
     </div><!--end of #header-->
     <nav id="top-nav" class="clearfix">
         <ul class="external-links">
-            <li><a href="javascript:void(0);"><i class="icon-view-changes-log"></i><?php _e('VIEW CHANGE LOG', 'sampression');?></a></li>
-            <li><a href="javascript:void(0);"><i class="icon-theme-documentation"></i><?php _e('THEME DOCUMENTATION', 'sampression');?></a></li>
-            <li><a href="javascript:void(0);"><i class="icon-support-desk"></i><?php _e('VISIT SUPPORT DESK', 'sampression');?></a></li>
+            <li><a href="javascript:void(0);"><i class="icon-view-changes-log"></i><?php _e( 'VIEW CHANGE LOG', 'sampression' );?></a></li>
+            <li><a href="javascript:void(0);"><i class="icon-theme-documentation"></i><?php _e( 'THEME DOCUMENTATION', 'sampression' );?></a></li>
+            <li><a href="javascript:void(0);"><i class="icon-support-desk"></i><?php _e( 'VISIT SUPPORT DESK', 'sampression' );?></a></li>
         </ul>
     </nav>
     <!-- #top-nav-->
