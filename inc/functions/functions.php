2c8cd726a332b08484eb558d5b7db0c3b13bd5d7
Bijay Yadav <bijay@idealaya.com.np>
2014-04-18 17:36:57 +0545

    title filter added

--- inc/functions/functions.php
+++ inc/functions/functions.php
@@ -58,6 +58,32 @@ endif;
 // Remove text color optopn from header options
 define( 'NO_HEADER_TEXT', true );
 
+/**
+ * Displays title. @uses wp_title() 
+ */
+add_filter( 'wp_title', 'sampression_filter_wp_title', 10, 2 );
+
+function sampression_filter_wp_title( $title, $sep = '|' ){
+        global $paged, $page;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the site name.
+	$title .= get_bloginfo( 'name' );
+
+	// Add the site description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title = "$title $sep $site_description";
+
+	// Add a page number if necessary.
+	if ( $paged >= 2 || $page >= 2 )
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'sampression' ), max( $paged, $page ) );
+
+	return $title;
+}
+
 /*
  * Sampression - Social Media Icons
  * @param $location header / footer
