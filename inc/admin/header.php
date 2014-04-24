7ee978952dfde796009fdbb51492c76b2270c638
Bijay Yadav <bijay@idealaya.com.np>
2014-04-24 14:09:27 +0545

    escape functions updated

--- inc/admin/header.php
+++ inc/admin/header.php
@@ -6,15 +6,14 @@
             <span class="tagline"><?php _e( 'a new theme', 'sampression' );?></span>
         </div>
         <div class="sam-version">
-            <span><?php _e( 'version 1.0', 'sampression' );?></span>
-            <span><?php _e( 'framework 1.0', 'sampression' );?></span>
+            <span><?php _e( 'version 1.4', 'sampression' );?></span>
         </div>
     </div><!--end of #header-->
     <nav id="top-nav" class="clearfix">
         <ul class="external-links">
-            <li><a href="http://sampression.com/themes/naya-lite" target="_blank"><i class="icon-view-changes-log"></i><?php _e( 'VIEW CHANGE LOG', 'sampression' );?></a></li>
-            <li><a href="http://docs.sampression.com/category/naya-lite" target="_blank"><i class="icon-theme-documentation"></i><?php _e( 'THEME DOCUMENTATION', 'sampression' );?></a></li>
-            <li><a href="http://sampression.com/forums/forum/naya-lite" target="_blank"><i class="icon-support-desk"></i><?php _e( 'VISIT SUPPORT DESK', 'sampression' );?></a></li>
+            <li><a href="<?php esc_url(__('http://sampression.com/themes/naya-lite','sampression')); ?>" target="_blank"><i class="icon-view-changes-log"></i><?php _e( 'VIEW CHANGE LOG', 'sampression' );?></a></li>
+            <li><a href="<?php esc_url(__('http://docs.sampression.com/category/naya-lite','sampression')); ?>" target="_blank"><i class="icon-theme-documentation"></i><?php _e( 'THEME DOCUMENTATION', 'sampression' );?></a></li>
+            <li><a href="<?php esc_url(__('http://sampression.com/forums/forum/naya-lite','sampression')); ?>" target="_blank"><i class="icon-support-desk"></i><?php _e( 'VISIT SUPPORT DESK', 'sampression' );?></a></li>
         </ul>
     </nav>
     <!-- #top-nav-->
--- inc/admin/sam-custom_css.php
+++ inc/admin/sam-custom_css.php
@@ -7,7 +7,7 @@ $css_settings = sampression_custom_css();
 <div id="content">
     <form id="sampression-metadata" onsubmit="javascript:return false;">
         <input type="hidden" name="meta_data" value="custom_css_settings" />
-        <textarea id="sam-custom-code" name="code"><?php echo $css_settings['css'] ?></textarea>
+        <textarea id="sam-custom-code" name="code"><?php echo esc_attr( $css_settings['css'] ); ?></textarea>
         <div id="response"></div>
         <a id="save" href="javascript:void(0);" class="button1 alignright save-data"><?php _e( 'Save', 'sampression' );?></a>
     </form>
