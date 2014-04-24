31512d2a1928ba606052d937c4c80acb7d03a471
Bijay Yadav <bijay@idealaya.com.np>
2014-04-24 09:28:33 +0545

    google font droid enqueued for theme option

--- inc/classes/admin.php
+++ inc/classes/admin.php
@@ -94,6 +94,7 @@ class Sampression_Admin {
         wp_enqueue_style( 'wp-color-picker' );
         wp_enqueue_style( 'admin-style', SAM_FW_ADMIN_CSS_URL . '/admin-style.css', false, false, 'screen' );
         wp_enqueue_style( 'font-style', '//fonts.googleapis.com/css?family=Kreon', false, false, 'screen' );
+        wp_enqueue_style( 'font-style', '//fonts.googleapis.com/css?family=Droid+Serif:400,400italic', false, false, 'screen' );
     }
 
     /**
